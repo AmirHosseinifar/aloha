37987d370b74508ff84d2fd09f070e86116b8779
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-04-28 08:05:23 -0700

    Aloha Project

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function(){
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+if (location.pathname.replace(/^\//,'')) == this.pathname.replace(/^\//,"") && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.slice(1)+']');
+  if (target.length) {
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 500);
+    return false;
+    
+  }
+}
+
+});
+
+$('.carousel ul').flickity({
+cellSelector: '.cell',
+cellAlign: 'center', 
+contain: true
+});
+
+$('#sign-up-form').on('submit', function(event){
+event.preventDefault();
+if ($('#singUp').val()== ''){
+  alert("Please enter a valid email address")
+}else{
+  alert('Thanks for subscribing')
+}
+});
+})
